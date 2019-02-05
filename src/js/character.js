1745db6d0e61eb51af6a032cac15257862e0e3ae
evb0110 <7149553@gmail.com>
2019-02-05 21:27:54 +0300

    modified the function itself

--- src/js/character.js
+++ src/js/character.js
@@ -4,12 +4,13 @@
  * @param {string} name - character's name
  * @param {string} type - character's type
  * 
+ * @throws Will throw if name isn't a string
  * @throws Will throw an error if name.length < 2
  * @throws Will throw an error if name.length > 10
- * @throws Will throw an error if name isn't a string
- * @throws Will throw if type doesn't belong to a predefined array of types
  */ 
 function Character(name, type) {
+  if (typeof name !== 'string') throw new Error('name should be a string');
+  if (name.length < 2 || name.length > 10) throw new Error('name has wrong length');
   this.name = name;
   this.type = type;
 }
